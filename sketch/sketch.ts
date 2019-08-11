932303a0fd863e32d753d11e348ef0a886caad48
S. Hall <shall.usnavy@gmail.com>
2019-08-11 17:32:46 -0400

    Make it even more bare bones

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,15 +1,31 @@
-let morph: Morph;
-function setup() {
-    createCanvas(windowWidth, windowHeight)
-    morph = new Morph();
-    morph.setup();
-}
+const sketch = (p: p5) => {
+  this.x = 100;
+  this.y = 100;
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
+  p.draw = () => {
+    p.background(0);
+    p.fill(255);
+    p.rect(this.x, this.y, 50, 50);
+  };
+};
 
-function draw() {
-    background(100);
-    morph.draw();
-}
+new p5(sketch);
+
+const smurf = (p: p5) => {
+  this.x = 100;
+  this.y = 100;
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = () => {
+    p.background(200);
+    p.fill(255);
+    p.rect(this.x, this.y, 150, 100);
+  };
+};
+
+new p5(smurf);
